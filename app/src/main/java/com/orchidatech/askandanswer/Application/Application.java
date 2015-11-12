4fe00f08c58f9bbda49a6c99932474398a2871b4
bahaa_sharqawe <bahaa_sharqawe@orchidatech.com>
2015-11-12 13:32:55 +0200

    12/11/2015 BahaaIddin Al Sharqawi

--- app/src/main/java/com/orchidatech/askandanswer/Application/Application.java
+++ app/src/main/java/com/orchidatech/askandanswer/Application/Application.java
@@ -6,11 +6,21 @@ import com.activeandroid.ActiveAndroid;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.orchidatech.askandanswer.Constant.Constants;
+import com.sromku.simple.fb.Permission;
+import com.sromku.simple.fb.SimpleFacebook;
+import com.sromku.simple.fb.SimpleFacebookConfiguration;
 
 /**
  * Created by Bahaa on 7/11/2015.
  */
 public class Application extends android.app.Application {
+    private static final String APP_ID = "";
+    private static final String APP_NAMESPACE = "";
+    private static final Permission[] permissions = new Permission[] {
+            Permission.USER_PHOTOS,
+            Permission.EMAIL,
+            Permission.PUBLISH_ACTION
+    };;
     @Override
     public void onCreate() {
         super.onCreate();
@@ -20,6 +30,11 @@ public class Application extends android.app.Application {
 
         ActiveAndroid.initialize(this);
 
-
+        SimpleFacebookConfiguration configuration = new SimpleFacebookConfiguration.Builder()
+                .setAppId(APP_ID)
+                .setNamespace(APP_NAMESPACE)
+                .setPermissions(permissions)
+                .build();
+        SimpleFacebook.setConfiguration(configuration);
     }
 }
