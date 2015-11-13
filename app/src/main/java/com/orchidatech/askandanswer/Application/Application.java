e9bf38503a0fdbae1ae62cd7a1a42e6920e6378c
bahaa_sharqawe <bahaa_sharqawe@orchidatech.com>
2015-11-13 12:33:46 +0200

    13/11/2015 BahaaIddin Al Sharqawi

--- app/src/main/java/com/orchidatech/askandanswer/Application/Application.java
+++ app/src/main/java/com/orchidatech/askandanswer/Application/Application.java
@@ -3,6 +3,7 @@ package com.orchidatech.askandanswer.Application;
 import android.content.SharedPreferences;
 
 import com.activeandroid.ActiveAndroid;
+import com.activeandroid.Configuration;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.orchidatech.askandanswer.Constant.Constants;
@@ -28,7 +29,12 @@ public class Application extends android.app.Application {
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
         ImageLoader.getInstance().init(config);
 
-        ActiveAndroid.initialize(this);
+        Configuration.Builder builder = new Configuration.Builder(getBaseContext());
+//        builder.setCacheSize(1024*1024*4);
+//        builder.setDatabaseName("AskAndAnswer.db");
+//        builder.setDatabaseVersion(1);
+        ActiveAndroid.initialize(builder.create(), true);
+//        ActiveAndroid.initialize(this);
 
         SimpleFacebookConfiguration configuration = new SimpleFacebookConfiguration.Builder()
                 .setAppId(APP_ID)
@@ -36,5 +42,11 @@ public class Application extends android.app.Application {
                 .setPermissions(permissions)
                 .build();
         SimpleFacebook.setConfiguration(configuration);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        ActiveAndroid.dispose();
     }
 }
--- app/src/main/java/com/orchidatech/askandanswer/Constant/Constants.java
+++ app/src/main/java/com/orchidatech/askandanswer/Constant/Constants.java
@@ -13,13 +13,42 @@ import java.util.TimeZone;
  */
 public class Constants {
 
+    public final static class Enum {
+
+            public enum NOTIFICATIONS {//Enum.NOTIFICATIONS.NEW_POST_ADDED.getNumericType();
+                NEW_POST_ADDED(0),
+                NEW_MENTION(1),
+                NEW_COMMENT(2);
+                private int type;
+
+                NOTIFICATIONS(int i) {
+                    this.type = i;
+                }
+
+                public int getNumericType() {
+                    return type;
+                }
+            }
+            public enum USER_ACTIONS {//Enum.USER_ACTIONS.LIKE.getNumericType();
+                NO_ACTIONS(0),
+                LIKE(1),
+                DISLIKE(2);
+                private int type;
+                    USER_ACTIONS(int i) {
+                    this.type = i;
+                }
+
+                public int getNumericType() {
+                    return type;
+                }
+        }
+}
     public static final String SHARED_PREF_NAME = "AskAndAnswerPref";
 
-    public static class DateConversion {
+    public final static class DateConversion {
         final static String MONTHS[] = new String[]{"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-        public static String getDate(long milliSeconds)
-        {
+        public static String getDate(long milliSeconds) {
             Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis(milliSeconds);
             String fullDate = MONTHS[calendar.get(Calendar.MONTH)] + " " + buildValueOf(calendar.get(Calendar.DAY_OF_MONTH)) +", " + calendar.get(Calendar.YEAR);
