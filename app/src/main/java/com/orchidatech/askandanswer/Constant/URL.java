2616d23230791ebd005a05ff79a63ca6a58f5cc3
bahaa_sharqawe <bahaa_sharqawe@orchidatech.com>
2015-11-25 22:40:47 +0200

    25/11/2015 BahaaIddin Al Sharqawi

--- app/src/main/java/com/orchidatech/askandanswer/Constant/URL.java
+++ app/src/main/java/com/orchidatech/askandanswer/Constant/URL.java
@@ -11,27 +11,27 @@ import java.util.Map;
  */
 public class URL {
     public static final String SERVER_URL = "http://softplaystore.com";
-    public static final String WEB_SERVICES_PATH = "sharearide/web-services/";
-
-    public static final String LOGIN = SERVER_URL + WEB_SERVICES_PATH + "login.php";
-    public static final String GET_USER_POSTS = SERVER_URL + WEB_SERVICES_PATH + "userpost.php";
-    public static final String GET_CATEGORIES = SERVER_URL + WEB_SERVICES_PATH + "all-categories.php";
-    public static final String REGISTER = SERVER_URL + WEB_SERVICES_PATH + "reg-user.php";
-    public static final String UPDATE_PROFILE = SERVER_URL + WEB_SERVICES_PATH + "update_profile.php";
-    public static final String SEND_USER_CATEGORIES = SERVER_URL + WEB_SERVICES_PATH + "add-user-category.php";
+    public static final String WEB_SERVICES_PATH = "/askandanswer_ws/public/";
     public static final String DEFAULT_IMAGE = "";
-    public static final String ADD_POST_FAVORITE = "add-post-favorite.php";
-    public static final String EDIT_POST = "edit-post.php";
-    public static final String ADD_POST = "add-post.php";
-    public static final String DELETE_POST = "delete-post.php";
-    public static final String GET_USER_FAV_POSTS = "userfavpost.php";
-    public static final String REMOVE_POST_FAVORITE = "remove-post-favorite.php";
-    public static final String GET_USER_INFO = "user_id.php";
-    public static final String GET_TIME_LINE = "newestpost.php";
-    public static final String SEARCH = "";
-    public static final String ADD_COMMENT = "add-comment.php";
+
+    public static final String LOGIN = SERVER_URL + WEB_SERVICES_PATH + "user/login.php";
+    public static final String REGISTER = SERVER_URL + WEB_SERVICES_PATH + "user/reg-user.php";
+    public static final String GET_USER_INFO = "user/user_info.php";
+    public static final String GET_CATEGORIES = SERVER_URL + WEB_SERVICES_PATH + "cat/all-categories.php";
+    public static final String UPDATE_PROFILE = SERVER_URL + WEB_SERVICES_PATH + "user/update_profile.php";
+    public static final String SEND_USER_CATEGORIES = SERVER_URL + WEB_SERVICES_PATH + "usercat/add-user-category.php";
+    public static final String GET_USER_POSTS = SERVER_URL + WEB_SERVICES_PATH + "post/userpost.php";
+    public static final String EDIT_POST = "post/edit-post.php";
+    public static final String ADD_POST = "post/add-post.php";
+    public static final String DELETE_POST = "post/delete-post.php";
+    public static final String GET_TIME_LINE = "post/newestpost.php";
+    public static final String ADD_POST_FAVORITE = "postFav/add-post-favorite.php";
+    public static final String GET_USER_FAV_POSTS = "postFav/userfavpost.php";
+    public static final String REMOVE_POST_FAVORITE = "postFav/remove-post-favorite.php";
+    public static final String SEARCH = "post/searchuserpostall.php";
+    public static final String ADD_COMMENT = "comment/add-comment.php";
     public static String GET_USER_Comments = "";
-    public static String GET_POST_Comments = "";
+    public static String GET_POST_Comments = "comment/post-comment.php";
 
     public class URLParameters {
         public static final String ID = "id";
@@ -56,5 +56,4 @@ public class URL {
         public static final String IS_HIDDEN = "is_hidden";
         public static final String COMMENT = "comment";
     }
-
 }
